3d14670b7f0d4f803f917be92bb919f1469df826
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-02-12 13:15:12 +0530

    sample

--- Hiru_RDS/test.js
+++ Hiru_RDS/test.js
@@ -21,28 +21,7 @@ exports.handler = function (event, context, callback) {
 			console.log(err, err.stack); // an error occurred
 		});
 
-	s3.getObject({
-		'Bucket': "hiru.sample",
-		'Key': "test"
-	}).promise()
-		.then(data => {
-			console.log(data);           // successful response
-			/*
-			data = {
-				AcceptRanges: "bytes", 
-				ContentLength: 3191, 
-				ContentType: "image/jpeg", 
-				ETag: "\\"6805f2cfc46c0f04559748bb039d69ae\\"", 
-				LastModified: <Date Representation>, 
-				Metadata: {...}, 
-				TagCount: 2, 
-				VersionId: "null"
-			}
-			*/
-		})
-		.catch(err => {
-			console.log(err, err.stack); // an error occurred
-		});
+
 
 
 	callback(null, 'Successfully executed');
