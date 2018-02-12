8a52b1b98425ae8e6994ac700b7586bb89a437b3
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-02-12 13:05:06 +0530

    test

---
+++ Hiru_RDS/test.js
@@ -0,0 +1,26 @@
+let AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+exports.handler = function (event, context, callback) {
+	s3.copyObject({
+		'Bucket': "hiru.sample",
+		'CopySource': "/hiru.sample.demo01/test",
+		'Key': "test"
+	}).promise()
+		.then(data => {
+			console.log(data);           // successful response
+			/*
+			data = {
+				CopyObjectResult: {
+					ETag: "\"6805f2cfc46c0f04559748bb039d69ae\"",
+					LastModified: <Date Representation>
+				}
+			}
+			*/
+		})
+		.catch(err => {
+			console.log(err, err.stack); // an error occurred
+		});
+
+
+	callback(null, 'Successfully executed');
+}
